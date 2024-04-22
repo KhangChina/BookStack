d2d70159f2db9e5a605c4bd057ca67cbda95b2ea
Khang China <khang.nguyen@htgsoft.com>
2024-04-22 16:43:53 +0700

    update docker

--- dev/docker/entrypoint.app.sh
+++ dev/docker/entrypoint.app.sh
@@ -7,9 +7,10 @@ env
 if [[ -n "$1" ]]; then
     exec "$@"
 else
-    composer install
+    composer install 
     wait-for-it db:3306 -t 45
-    php artisan migrate --database=mysql
+    php artisan migrate --database=mysql #cần ấn yes
     chown -R www-data:www-data storage
+    php artisan key:generate #cần ấn yes
     exec apache2-foreground
 fi
